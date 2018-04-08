68954b8e357e6b378cc51c8e2000ca89efb59fcd
zhangjie <chulinice@sina.com>
2018-04-08 18:07:20 +0800

    index md

---
+++ src/static/js/index.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  // var iframes = document.getElementById('iframes');
+  // iframes.src = '/pages/score-item-two.html';
+  //
+  // var navlis = document.getElementById('navs').querySelectorAll('li')
+  // navlis.forEach(function (item) {
+  //   item.addEventListener('click', function (event) {
+  //     var target = event.target
+  //     var href = target.getAttribute('data-href')
+  //     iframes.src = href
+  //   })
+  // })
+}
